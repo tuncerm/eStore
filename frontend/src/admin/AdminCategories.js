9d5f1ae4afc1f78a79ed30dc085de172cabb9d37
Mustafa Tuncer <tuncerm@gmail.com>
2021-02-20 00:57:01 +0100

    Update: Used new Table Component. Added Form state for future use.

--- frontend/src/admin/AdminCategories.js
+++ frontend/src/admin/AdminCategories.js
@@ -1,45 +1,74 @@
 import React from 'react';
 
+import Table, {TableHeader, TableRow} from '../shared/UIElements/Table';
+
+import { useForm } from '../shared/hooks/form-hook';
+import { useHttpClient } from '../shared/hooks/http-hook';
+
 import './AdminCategories.css';
 
 const AdminCategories = () => {
+  const {isLoading, error, sendRequest, clearError} = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: '',
+        isValid: false
+      },
+      details: {
+        value: '',
+        isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const DUMMYDATA = [
+    {name:"Cat", details:"It is a cat.", image:"cat_image.png"},
+    {name:"Cat", details:"It is a cat.", image:"cat_image.png"},
+    {name:"Cat", details:"It is a cat.", image:"cat_image.png"},
+    {name:"Cat", details:"It is a cat.", image:"cat_image.png"}
+  ]
+
+  const createCategory = async event => {
+    event.preventDefault();
+    try{
+      const formData = new FormData();
+      formData.append('name', formState.inputs.name.value);
+      formData.append('details', formState.inputs.details.value);
+      formData.append('image', formState.inputs.image.value);
+      // await sendRequest(`${process.env.REACT_APP_API_URL}/admin/category`, 'POST', formData, {Authorization: 'Bearer ' + auth.token});
+      // history.push('/');
+    } catch {}
+  };
+
+
   return (
     <div className="admin-categories">
       <p>The Categories</p>
-      <div className="admin-categories-new">
-        <input type="text" placeholder="Title"/>
-        <input type="text" placeholder="Description"/>
-        <input type="file"/>
+      <form className="admin-categories-new" onSubmit={createCategory}>
+        <input name="name" type="text" placeholder="Title"/>
+        <input name="details" type="text" placeholder="Description"/>
+        <input name="image" type="file"/>
         <input type="submit" value="Save"/>
-      </div>
-      <table class="table-dark table-striped">
-        <thead>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Image</th>
-          <th>Action</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Cat</td>
-            <td>It is a cat.</td>
-            <td>image.jpg</td>
-            <td>Edit/Delete</td>
-          </tr>
-          <tr>
-            <td>Cat</td>
-            <td>It is a cat.</td>
-            <td>image.jpg</td>
-            <td>Edit/Delete</td>
-          </tr>
-          <tr>
-            <td>Cat</td>
-            <td>It is a cat.</td>
-            <td>image.jpg</td>
-            <td>Edit/Delete</td>
-          </tr>
-        </tbody>
-      </table>
+      </form>
+
+      <Table
+        isStriped
+        isDark
+        header={<TableHeader th={["Title", "Description", "Image", "Action"]}/>} 
+        rows={DUMMYDATA.map(v=><TableRow td={[
+          v.name,
+          v.details,
+          v.image, 
+          <button>Jhonny</button>
+        ]}/>)}
+      />
+
     </div>
   )
 };
