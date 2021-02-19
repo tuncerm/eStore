5dc43188591d122628e1c6a3dc30813a96f919fc
Mustafa Tuncer <tuncerm@gmail.com>
2021-02-20 00:58:01 +0100

    Updated Product Schema.

--- server/models/product.js
+++ server/models/product.js
@@ -1,11 +1,13 @@
 const {model, Schema, Types} = require('mongoose');
 
 const thisSchema = new Schema({
+    category:{type: Types.ObjectId, ref: 'Category'},
+    tags: [{type:Types.ObjectId, ref:'Tag'}],
     name: {type: String, required: true},
     price: {type: Number, required: true},
     currency: {type: String, required: true},
     details: {type: String, required: true},
-    images: [String]
+    images: [{url:String, uuid:String}]
 });
 
 module.exports = model('Product', thisSchema);
