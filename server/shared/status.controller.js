b8350c1074bdb64250bbb7e7f1870045be524850
Mustafa Tuncer <tuncerm@gmail.com>
2021-02-20 00:59:18 +0100

    Update status controllers. Added notFound handler.

--- server/shared/status.controller.js
+++ server/shared/status.controller.js
@@ -1,7 +1,12 @@
 const notAcceptable = (req, res)=>{
-    res.status(406).json("Not acceptable.");
+    res.status(406).json({message: "Not acceptable."});
+};
+
+const notFound = (req, res)=>{
+    res.status(404).json({message: "Not found."});
 };
 
 module.exports = {
-    notAcceptable
+    notAcceptable,
+    notFound
 }
