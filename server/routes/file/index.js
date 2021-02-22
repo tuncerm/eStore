c7a194b840215b3e9279ff2dc211cfb0b941e3e5
Mustafa Tuncer <tuncerm@gmail.com>
2021-02-22 22:37:39 +0100

    Featrue: Completed endpoint for file upload!

--- server/routes/file/index.js
+++ server/routes/file/index.js
@@ -4,11 +4,12 @@ const router = express.Router();
 
 const {getFiles, getFile, uploadSingle, uploadMultiple, deleteFile} = require("./file.controller");
 const {notAcceptable} = require("../../shared/status.controller");
+const fileUpload = require('../../middlewares/file-upload');
 
 router.get("/", getFiles);
 router.get("/:fileID", getFile);
-router.post("/", uploadSingle);
-router.post("/multi", uploadMultiple);
+router.post("/", fileUpload.single('file'), uploadSingle);
+router.post("/multi", fileUpload.array('file', 10), uploadMultiple);
 router.delete("/:fileID", deleteFile);
 
 router.use(notAcceptable);
