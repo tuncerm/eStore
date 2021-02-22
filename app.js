c74e33c3c1e2070d39004d36a3c72b9885578b6a
Mustafa Tuncer <tuncerm@gmail.com>
2021-02-22 22:35:33 +0100

    Added cors.

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 
 const routes = require('./server/routes');
 const frontendApp = require('./server/tools/serve-frontend');
 
-const PORT = process.env.PORT || 3310;
-
-const {DB_USER, DB_PSW, DB_HOST, DB_NAME } = process.env;
+const {DB_USER, DB_PSW, DB_HOST, DB_NAME, PORT = 3310 } = process.env;
+const URL = `mongodb+srv://${DB_USER}:${DB_PSW}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use('/api', routes);
-
 app.use(frontendApp);
-
-const URL = `mongodb+srv://${DB_USER}:${DB_PSW}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose
     .connect(URL, {useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>{
         app.listen(PORT);
+        console.log(`Listening on port: ${PORT}`);
     })
     .catch(console.error);
