ae8678f7d82e12dcff4f3cf2c7175dbbdfd4034b
Mustafa Tuncer <tuncerm@gmail.com>
2021-02-22 01:53:34 +0100

    Edit: Implemented a button component for general use.

--- frontend/src/shared/UIElements/Button.js
+++ frontend/src/shared/UIElements/Button.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import './Button.css';
 
-const Button = () => {
+const Button = ({children, size, inverse, danger, type = "button", onClick, disabled}) => {
   return (
-    <>
-    
-    </>
+    <button className={`btn btn-${size || 'md'} ${inverse &&
+        'btn-inverse'} ${danger && 'btn-danger'}`}
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
   )
 };
 
